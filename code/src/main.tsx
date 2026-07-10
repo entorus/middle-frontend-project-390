65ee5fcf831c3f2d040fd61db5670b9ce61d56a4
entorus <entorus@yandex.ru>
2026-07-10 09:56:37 +0900

    add test for bugsink

--- code/src/main.tsx
+++ code/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import App from './App.tsx'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+Sentry.init({
+  dsn: 'https://41ff33b0ab914764b9bd5b6a80516324@booking.bugsink.com/1',
+
+  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+  // if your build tool supports it.
+  release: 'booking-hexlet',
+
+  integrations: [],
+  tracesSampleRate: 0,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- code/src/pages/SearchFlightsPage.tsx
+++ code/src/pages/SearchFlightsPage.tsx
@@ -25,6 +25,7 @@ export default function SearchFlightsPage () {
 
   const searchFlights = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault()
+    throw new Error('Error Thrown on purpose to send it to Bugsink')
     const form = e.currentTarget
 
     if (!form.checkValidity()) {
